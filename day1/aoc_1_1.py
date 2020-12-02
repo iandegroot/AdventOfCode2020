2f228edbc07f52d9c251fc675abc081a108c8754
Ian DeGroot <ian.degroot@cirrus.com>
2020-12-02 16:23:04 -0600

    Solutions for Day 1.

---
+++ day1/aoc_1_1.py
@@ -0,0 +1,15 @@
+
+numbers = []
+
+with open('input.txt') as input_file:
+    for line in input_file:
+        numbers.append(int(line.strip()))
+
+print(numbers)
+
+for i in range(len(numbers)):
+    for j in range(i, len(numbers)):
+        if (numbers[i] + numbers[j] == 2020):
+            print("The numbers are {} and {}".format(numbers[i], numbers[j]))
+            print("The answer is {}".format(numbers[i] * numbers[j]))
+
---
+++ day1/aoc_1_2.py
@@ -0,0 +1,17 @@
+
+numbers = []
+
+with open('input.txt') as input_file:
+    for line in input_file:
+        numbers.append(int(line.strip()))
+
+print(numbers)
+
+for i in range(len(numbers)):
+    for j in range(i, len(numbers)):
+        if (numbers[i] + numbers[j] < 2020):
+            for k in range(j, len(numbers)):
+                if (numbers[i] + numbers[j] + numbers[k] == 2020):
+                    print("The numbers are {}, {}, and {}".format(numbers[i], numbers[j], numbers[k]))
+                    print("The answer is {}".format(numbers[i] * numbers[j] * numbers[k]))
+
